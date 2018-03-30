cde00c6c54acc7bddf0c9d95fbd351879da48c10
Orkhan <orkhankhf@code.edu.az>
2018-03-30 14:27:48 +0400

    API System files added again to repo

---
+++ CodeAcademyAttendanceSystemAPI 16-03-2018 19-47/CodeAcademyAttendanceSystemAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace CodeAcademyAttendanceSystemAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+
+    }
+}
